5c213a2d580e450b0cb6b08322737fd361fced3a
Ikjot Kaur <jot@ikjots-mbp.mobile.asu.edu>
2018-02-05 16:59:29 -0700

    Added the download image function

--- RolodexApp/RolodexApp/Controller/ViewController.swift
+++ RolodexApp/RolodexApp/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -24,7 +25,7 @@ class ViewController: UIViewController {
         Person.getPersonData { (person) in
             self.persons = person
             self.myCollectionView.reloadData()
-            self.downloadPersonImage(persons: person, completion: { (images) in
+            self.downloadPersonImage(persons: self.persons, completion: { (images) in
                 self.personImages = images
                 self.myCollectionView.reloadData()
             })
@@ -72,23 +73,36 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource{
     
     func downloadPersonImage(persons:[Person],completion: @escaping ([UIImage])->()){
         var images:[UIImage] = [UIImage]()
+        let downloadGroup = DispatchGroup()
         for person in persons{
             guard let personURL = URL(string: person.avatar) else {
                 return}
-            URLSession.shared.dataTask(with: personURL) { (data, response, error) in
-                if error != nil{
-                    print("download image function failed")
+//            URLSession.shared.dataTask(with: personURL) { (data, response, error) in
+//                downloadGroup.enter()
+//                if error != nil{
+//                    print("download image function failed")
+//                }
+//                guard let unrappedData = data else {
+//                    return}
+//                if let image = UIImage(data: unrappedData) {
+//                    images.append(image)
+//                }
+//                downloadGroup.leave()
+//            }.resume()
+            downloadGroup.enter()
+            Alamofire.request(personURL).responseData(completionHandler: { (data) in
+                if let data = data.data {
+                    if let image = UIImage(data: data) {
+                        images.append(image)
+                        downloadGroup.leave()
+                    }
                 }
-                guard let unrappedData = data else {
-                    return}
-                if let image = UIImage(data: unrappedData) {
-                    images.append(image)
-                }
-            }
+            })
         }
-        DispatchQueue.main.async {
-            completion(images)
+        downloadGroup.notify(queue: DispatchQueue.main) { // 2
+        completion(images)
         }
-}
+
+        }
 }
 
